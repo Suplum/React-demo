d2c1809c2edcf798156485ea73f73238f39651bf
Suplum <799819167@qq.com>
2022-03-15 17:40:55 +0800

    debounce

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import qs from "qs"
-import { cleanObject } from "utils"
+import { cleanObject, useDebounce } from "utils"
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
@@ -12,26 +12,33 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+  const debouncedParam = useDebounce(param, 2000)
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [param])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}/>
     <List users={users} list={list}/>
   </div>
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 }
--- jira/src/utils/index.js
+++ jira/src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -11,4 +12,39 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if(timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay);
+//   }
+// }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
+
+// debounce 原理讲解（闭包原理）
+// 0s ---> 1s ---> 2s ---> ...
+// 一定要理解：这三个函数都是同步操作，所以它们都是在 0-1s 这个时间段内瞬间完成的
+// log()#1 // timeout#1
+// log()#2 // 发现timeout#1 取消之，然后设置timeout#2
+// log()#3 // 发现timeout#2 取消之，然后设置timeout#3
+// 所以，log()#3 结束后，就只剩timeout#3在独自等待了
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };

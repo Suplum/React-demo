fceec7961c7ee4a1a26907495b32fd55c139c9cf
Suplum <799819167@qq.com>
2022-03-20 20:31:14 +0800

    作业

--- jira/src/utils/index.ts
+++ jira/src/utils/index.ts
@@ -59,3 +59,16 @@ export const useMount = (callback: () => void) => {
     callback();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 };
+
+export const useArray = <V>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
